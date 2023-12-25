4f688f7d763bc615a41f621872b9c7b98d379917
JMsuper <tjsqls2067@naver.com>
2023-12-25 13:42:11 +0900

    add command pattern

---
+++ xunit/src/junit/TestCase.java
@@ -0,0 +1,14 @@
+package junit;
+
+public class TestCase implements Test{
+    private final String fName;
+
+    public TestCase(String name) {
+        this.fName = name;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
